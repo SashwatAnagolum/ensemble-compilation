65f5d00adbb0d74b65189298f152340fe7f99f95
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2024-06-11 16:33:34 +0530

    print pwd during tests

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -12,7 +12,7 @@ config.suffixes = [".mlir"]
 
 # lit executes relative to the directory
 #
-#   bazel-bin/tests/<test_target_name>.runfiles/mlir_tutorial/
+#   bazel-bin/tests/<test_target_name>.runfiles/ensemble-compilation/
 #
 # which contains all the binary targets included in via the `data` attribute in
 # the lit.bzl macro, which in turn gets them from the filegroup //tests:test_utilities.
@@ -25,9 +25,9 @@ config.suffixes = [".mlir"]
 #   print(subprocess.run(["ls", "-l", os.environ["RUNFILES_DIR"]]).stdout)
 #   print(subprocess.run([ "env", ]).stdout)
 #
-# Bazel defines RUNFILES_DIR which includes mlir_tutorial/ and third party
+# Bazel defines RUNFILES_DIR which includes ensemble-compilation/ and third party
 # dependencies as their own directory. Generally, it seems that $PWD ==
-# $RUNFILES_DIR/mlir_tutorial/
+# $RUNFILES_DIR/ensemble-compilation/
 runfiles_dir = Path(os.environ["RUNFILES_DIR"])
 tool_relpaths = [
     "llvm-project/mlir",
@@ -39,3 +39,9 @@ config.environment["PATH"] = (
     + ":"
     + os.environ["PATH"]
 )
+
+import subprocess
+
+print(subprocess.run(["pwd",]).stdout)
+print(subprocess.run(["ls", "-l", os.environ["RUNFILES_DIR"]]).stdout)
+print(subprocess.run([ "env", ]).stdout)
