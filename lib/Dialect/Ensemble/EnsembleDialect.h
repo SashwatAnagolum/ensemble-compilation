5188de2674f4aebc7686d34de5cbd9dc80db0e78
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2024-06-13 14:31:50 +0530

    Fix include ordering for dialect headers

--- lib/Dialect/Ensemble/EnsembleDialect.h
+++ lib/Dialect/Ensemble/EnsembleDialect.h
@@ -1,9 +1,10 @@
 #ifndef LIB_DIALECT_ENSEMBLE_ENSEMBLEDIALECT_H_
 #define LIB_DIALECT_ENSEMBLE_ENSEMBLEDIALECT_H_
 
+#include "mlir/include/mlir/IR/DialectImplementation.h"
+
 // Required because the .h.inc file refers to MLIR classes and does not itself
 // have any includes.
 #include "lib/Dialect/Ensemble/EnsembleDialect.h.inc"
-#include "mlir/include/mlir/IR/DialectImplementation.h"
 
 #endif  // LIB_DIALECT_ENSEMBLE_ENSEMBLEDIALECT_H_
