fca7eb45a3f94dbef8db7d6fb03e42e705cfd474
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2024-06-14 22:35:41 +0530

    Add poly.add operation
    Add ensemble.1qgate and ensemble.2qgate ops
    
    Add header include guards for EnsembleDialect.td and PolyDialect.td
    
    Fix includes in EnsembleDialect.cpp
    
    Include EnsembleOps.cpp.inc in EnsembleDialect.cpp
    
    Debug EnsembleDialect
    
    Debug EnsembleDialect
    
    Change BUILD for Ensemble dialect
    
    Debug Ensemble dialect ops
    
    Update 1q and 2q gate ops
    
    Fix typo in EnsembleOps.td
    
    Fix typos in EnsembleOps.td
    
    Remove 2q gate op
    
    Remove 2q gate op
    
    Rework 1q gate o

--- lib/Dialect/Ensemble/EnsembleDialect.cpp
+++ lib/Dialect/Ensemble/EnsembleDialect.cpp
@@ -1,6 +1,10 @@
 #include "lib/Dialect/Ensemble/EnsembleDialect.h"
 
+#include "lib/Dialect/Ensemble/EnsembleOps.h"
 #include "lib/Dialect/Ensemble/EnsembleTypes.h"
+
+//
+
 #include "llvm/include/llvm/ADT/TypeSwitch.h"
 #include "mlir/include/mlir/IR/Builders.h"
 
@@ -9,16 +13,24 @@
 #include "lib/Dialect/Ensemble/EnsembleDialect.cpp.inc"
 #define GET_TYPEDEF_CLASSES
 #include "lib/Dialect/Ensemble/EnsembleTypes.cpp.inc"
+#define GET_OP_CLASSES
+#include "lib/Dialect/Ensemble/EnsembleOps.cpp.inc"
 
 namespace mlir {
 namespace qe {
 namespace ensemble {
 
 void EnsembleDialect::initialize() {
-  // This is where we will register types and operations with the dialect
+  // Add all of the types for the Ensemble dialect
   addTypes<
 #define GET_TYPEDEF_LIST
 #include "lib/Dialect/Ensemble/EnsembleTypes.cpp.inc"
+      >();
+
+  // Add all of the operations for the Ensemble dialect
+  addOperations<
+#define GET_OP_LIST
+#include "lib/Dialect/Ensemble/EnsembleOps.cpp.inc"
       >();
 }
 
--- lib/Dialect/Poly/PolyDialect.cpp
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -1,6 +1,10 @@
 #include "lib/Dialect/Poly/PolyDialect.h"
 
+#include "lib/Dialect/Poly/PolyOps.h"
 #include "lib/Dialect/Poly/PolyTypes.h"
+
+//
+
 #include "llvm/include/llvm/ADT/TypeSwitch.h"
 #include "mlir/include/mlir/IR/Builders.h"
 
@@ -9,16 +13,24 @@
 #include "lib/Dialect/Poly/PolyDialect.cpp.inc"
 #define GET_TYPEDEF_CLASSES
 #include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+#define GET_OP_CLASSES
+#include "lib/Dialect/Poly/PolyOps.cpp.inc"
 
 namespace mlir {
 namespace qe {
 namespace poly {
 
 void PolyDialect::initialize() {
-  // This is where we will register types and operations with the dialect
+  // Add all of the types for the Poly dialect
   addTypes<
 #define GET_TYPEDEF_LIST
 #include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+      >();
+
+  // Add all of the operations for the Poly dialect
+  addOperations<
+#define GET_OP_LIST
+#include "lib/Dialect/Poly/PolyOps.cpp.inc"
       >();
 }
 
