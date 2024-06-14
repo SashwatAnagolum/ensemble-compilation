65bc31833fe2c2b5cc122c94b252c05bad7aa25b
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2024-06-14 10:31:23 +0530

    Add poly type to Poly dialect; add Qubit type to Ensemble dialect

--- lib/Dialect/Ensemble/EnsembleDialect.cpp
+++ lib/Dialect/Ensemble/EnsembleDialect.cpp
@@ -1,6 +1,14 @@
 #include "lib/Dialect/Ensemble/EnsembleDialect.h"
 
+#include "lib/Dialect/Ensemble/EnsembleTypes.h"
+#include "llvm/include/llvm/ADT/TypeSwitch.h"
+#include "mlir/include/mlir/IR/Builders.h"
+
+//
+
 #include "lib/Dialect/Ensemble/EnsembleDialect.cpp.inc"
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Ensemble/EnsembleTypes.cpp.inc"
 
 namespace mlir {
 namespace qe {
@@ -8,6 +16,10 @@ namespace ensemble {
 
 void EnsembleDialect::initialize() {
   // This is where we will register types and operations with the dialect
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "lib/Dialect/Ensemble/EnsembleTypes.cpp.inc"
+      >();
 }
 
 }  // namespace ensemble
--- lib/Dialect/Poly/PolyDialect.cpp
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -1,6 +1,14 @@
 #include "lib/Dialect/Poly/PolyDialect.h"
 
+#include "lib/Dialect/Poly/PolyTypes.h"
+#include "llvm/include/llvm/ADT/TypeSwitch.h"
+#include "mlir/include/mlir/IR/Builders.h"
+
+//
+
 #include "lib/Dialect/Poly/PolyDialect.cpp.inc"
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Poly/PolyTypes.cpp.inc"
 
 namespace mlir {
 namespace qe {
@@ -8,6 +16,10 @@ namespace poly {
 
 void PolyDialect::initialize() {
   // This is where we will register types and operations with the dialect
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+      >();
 }
 
 }  // namespace poly
