d11f44469339537823d6fb80ba8676707cde4b48
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2024-06-16 16:09:49 +0530

    Add test for loop invariant code motion

--- tools/qe-opt.cpp
+++ tools/qe-opt.cpp
@@ -2,19 +2,25 @@
 #include "lib/Dialect/Poly/PolyDialect.h"
 #include "lib/Transform/Affine/Passes.h"
 #include "lib/Transform/Arith/Passes.h"
+
+//
+
 #include "mlir/include/mlir/InitAllDialects.h"
+#include "mlir/include/mlir/InitAllPasses.h"
 #include "mlir/include/mlir/Pass/PassManager.h"
 #include "mlir/include/mlir/Pass/PassRegistry.h"
 #include "mlir/include/mlir/Tools/mlir-opt/MlirOptMain.h"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
-  mlir::registerAllDialects(registry);
+
   registry.insert<mlir::qe::poly::PolyDialect>();
   registry.insert<mlir::qe::ensemble::EnsembleDialect>();
+  mlir::registerAllDialects(registry);
 
   mlir::qe::registerAffinePasses();
   mlir::qe::registerArithPasses();
+  mlir::registerAllPasses();
 
   return mlir::asMainReturnCode(mlir::MlirOptMain(
       argc, argv, "Quantum Ensemble Compilation Pass Driver", registry));
