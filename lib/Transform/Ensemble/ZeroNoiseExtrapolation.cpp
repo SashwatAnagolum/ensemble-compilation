82ad4465d1f1085780dc3ea025d7b3254b074c82
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2024-06-18 18:13:09 +0530

    debug

--- lib/Transform/Ensemble/ZeroNoiseExtrapolation.cpp
+++ lib/Transform/Ensemble/ZeroNoiseExtrapolation.cpp
@@ -52,7 +52,7 @@ struct AddGateAndAdjointPairs : public OpRewritePattern<Gate1QOp> {
       // modify the operands so that the original op's result
       // is fed into the new gate
       auto newGateOp = rewriter.clone(*op.getOperation());
-      auto numOperands = op.getOperation().getNumOperands();
+      auto numOperands = op.getOperation()->getNumOperands();
 
       std::cout << numOperands << "\n";
 
