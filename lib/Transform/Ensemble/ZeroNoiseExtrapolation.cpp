807cc2073a7e3180d0374c9b9f1d7c7cdc378521
SashwatAnagolum <sashwat.anagolum@gmail.com>
2024-06-19 17:43:36 +0000

    support Gate2q ops in ZNE pass

--- lib/Transform/Ensemble/ZeroNoiseExtrapolation.cpp
+++ lib/Transform/Ensemble/ZeroNoiseExtrapolation.cpp
@@ -25,70 +25,85 @@ using arith::ConstantOp;
 using mlir::affine::AffineForOp;
 using mlir::affine::AffineYieldOp;
 
+template <typename T>
+LogicalResult zneRewritePattern(T &op, PatternRewriter &rewriter) {
+  // if we have already processed the op, exit.
+  if (op->getAttr("zne-applied")) {
+    return failure();
+  } else {
+    // Save the current insertion point and get the op location
+    auto insertionPoint = rewriter.saveInsertionPoint();
+    auto loc = op.getLoc();
+    rewriter.setInsertionPointAfter(op);
+
+    // get the Operation object associated with the op
+    mlir::Operation *operation = op.getOperation();
+
+    // Insert an affine for loop
+    auto forOp = rewriter.create<AffineForOp>(loc, 0, 10, 1);
+
+    // Set the insertion point to the start of the loop body
+    rewriter.setInsertionPointToStart(forOp.getBody());
+
+    // Clone the original operation inside the loop body
+    // and create two copies of the original gate
+    mlir::Operation *newGateOp = rewriter.clone(*operation);
+    mlir::Operation *newGateOpAdjoint = rewriter.clone(*operation);
+
+    // mark the second copy as adjoint
+    newGateOpAdjoint->setAttr("adjoint", rewriter.getUnitAttr());
+
+    // set the operands of the first new gate as the results
+    // of the original gate
+    newGateOp->setOperands(operation->getResults());
+
+    // set the operands of the adjoint gate as the
+    // results of the first new gate
+    newGateOpAdjoint->setOperands(newGateOp->getResults());
+
+    // replaces all uses of the result of the original gate
+    // with the result of the new adjoint gate
+    int numResults = (int) operation->getNumResults();
+
+    for (int resultIndex = 0; resultIndex < numResults; resultIndex++) {
+      operation->replaceUsesOfWith(operation->getResult(resultIndex),
+                                   newGateOpAdjoint->getResult(resultIndex));
+    }
+
+    // mark the new gates as already processed
+    // to prevent infinite loops
+    newGateOp->setAttr("zne-applied", rewriter.getUnitAttr());
+    newGateOpAdjoint->setAttr("zne-applied", rewriter.getUnitAttr());
+
+    // Restore the original insertion point
+    rewriter.restoreInsertionPoint(insertionPoint);
+
+    rewriter.updateRootInPlace(
+        op, [&]() { op->setAttr("zne-applied", rewriter.getUnitAttr()); });
+
+    return success();
+  }
+}
+
 // Add an affine for loop after every gate op containing
 // the same gate and its adjoint.
-struct AddGateAndAdjointPairs : public OpRewritePattern<Gate1QOp> {
-  AddGateAndAdjointPairs(mlir::MLIRContext *context)
+struct AddGateAndAdjointPairs1Q : public OpRewritePattern<Gate1QOp> {
+  AddGateAndAdjointPairs1Q(mlir::MLIRContext *context)
       : OpRewritePattern<Gate1QOp>(context, /*benefit=*/1) {}
 
   LogicalResult matchAndRewrite(Gate1QOp op,
                                 PatternRewriter &rewriter) const override {
-    // if we have already processed the op, exit.
-    if (op->getAttr("zne-applied")) {
-      return failure();
-    } else {
-      // Save the current insertion point and get the op location
-      auto insertionPoint = rewriter.saveInsertionPoint();
-      auto loc = op.getLoc();
-      rewriter.setInsertionPointAfter(op);
+    return zneRewritePattern<Gate1QOp>(op, rewriter);
+  }
+};
 
-      // get the Operation object associated with the op
-      mlir::Operation *operation = op.getOperation();
+struct AddGateAndAdjointPairs2Q : public OpRewritePattern<Gate2QOp> {
+  AddGateAndAdjointPairs2Q(mlir::MLIRContext *context)
+      : OpRewritePattern<Gate2QOp>(context, /*benefit=*/1) {}
 
-      // Insert an affine for loop
-      auto forOp = rewriter.create<AffineForOp>(loc, 0, 10, 1);
-
-      // Set the insertion point to the start of the loop body
-      rewriter.setInsertionPointToStart(forOp.getBody());
-
-      // Clone the original operation inside the loop body
-      // and create two copies of the original gate
-      mlir::Operation *newGateOp = rewriter.clone(*operation);
-      mlir::Operation *newGateOpAdjoint = rewriter.clone(*operation);
-
-      // mark the second copy as adjoint
-      newGateOpAdjoint->setAttr("adjoint", rewriter.getUnitAttr());
-
-      // set the operands of the first new gate as the results
-      // of the original gate
-      newGateOp->setOperands(operation->getResults());
-
-      // set the operands of the adjoint gate as the
-      // results of the first new gate
-      newGateOpAdjoint->setOperands(newGateOp->getResults());
-
-      // replaces all uses of the result of the original gate
-      // with the result of the new adjoint gate
-      unsigned numResults = operation->getNumResults();
-
-      for (int resultIndex = 0; resultIndex < numResults; resultIndex++) {
-        operation->replaceUsesOfWith(operation->getResult(resultIndex),
-                                     newGateOpAdjoint->getResult(resultIndex));
-      }
-
-      // mark the new gates as already processed
-      // to prevent infinite loops
-      newGateOp->setAttr("zne-applied", rewriter.getUnitAttr());
-      newGateOpAdjoint->setAttr("zne-applied", rewriter.getUnitAttr());
-
-      // Restore the original insertion point
-      rewriter.restoreInsertionPoint(insertionPoint);
-
-      rewriter.updateRootInPlace(
-          op, [&]() { op->setAttr("zne-applied", rewriter.getUnitAttr()); });
-
-      return success();
-    }
+  LogicalResult matchAndRewrite(Gate2QOp op,
+                                PatternRewriter &rewriter) const override {
+    return zneRewritePattern<Gate2QOp>(op, rewriter);
   }
 };
 
@@ -98,7 +113,8 @@ struct ZeroNoiseExtrapolation
 
   void runOnOperation() {
     mlir::RewritePatternSet patterns(&getContext());
-    patterns.add<AddGateAndAdjointPairs>(&getContext());
+    patterns.add<AddGateAndAdjointPairs1Q>(&getContext());
+    patterns.add<AddGateAndAdjointPairs2Q>(&getContext());
 
     (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
   }
--- tests/ensemble_syntax.mlir
+++ tests/ensemble_syntax.mlir
@@ -5,8 +5,7 @@ module {
     %0 = ensemble.gate1q "H" %arg0 : (!ensemble.qubit) -> (!ensemble.qubit)
     %1 = ensemble.gate1q "H" %arg1 : (!ensemble.qubit) -> (!ensemble.qubit)
 
-    %2 = ensemble.gate1q "X" %0 : (!ensemble.qubit) -> (!ensemble.qubit)
-    %3 = ensemble.gate1q "X" %1 : (!ensemble.qubit) -> (!ensemble.qubit)
+    %2, %3 = ensemble.gate2q "CX" %0, %1 : (!ensemble.qubit, !ensemble.qubit) -> (!ensemble.qubit, !ensemble.qubit)
 
     return %3: !ensemble.qubit
   }
